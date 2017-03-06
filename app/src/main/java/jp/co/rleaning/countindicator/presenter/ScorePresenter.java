496312ad68e3aad79335a647f9e0e8c609ed83c8
RLCL-217 <s-kawamura@r-learning.co.jp>
2017-03-06 19:15:58 +0900

    完成

--- app/src/main/java/jp/co/rleaning/countindicator/presenter/ScorePresenter.java
+++ app/src/main/java/jp/co/rleaning/countindicator/presenter/ScorePresenter.java
@@ -1,53 +1,38 @@
 package jp.co.rleaning.countindicator.presenter;
 
 /**
- * Created by user on 2016/12/10.
+ * Created by RLCL-217 on 2016/12/15.
  */
 
 public interface ScorePresenter {
+
     /**
      * イニングの+ボタンを押した時
      */
     void onClickInningPlus();
+
 
     /**
      * イニングの-ボタンを押した時
      */
     void onClickInningMinus();
 
+
     /**
      * スコアの+ボタンを押した時
      */
     void onClickScorePlus();
 
-    /**
-     * スコアの-ボタンを押した時
-     */
+
     void onClickScoreMinus();
 
-    /**
-     * Bボタンを押した時
-     */
+
+    void onClear();
+
     void onClickB();
 
-    /**
-     * Sボタンを押した時
-     */
     void onClickS();
 
-    /**
-     * Oボタンを押した時
-     */
     void onClickO();
-
-    /**
-     * クリアボタンを押した時
-     */
-    void onClickClear();
-
-    /**
-     * チェックボタンを押した時
-     */
-    void onCheckedChangedGroup();
 
 }
